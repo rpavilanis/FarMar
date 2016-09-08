336c3557f35d52a2327365ec741768df9280ad4a
Rachel <rachel.l.pavilanis@gmail.com>
2016-09-08 13:53:32 -0700

    cleaned up market_spec.rb with let - all tests passing.

--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -3,10 +3,17 @@ require_relative '../lib/market'
 
 describe FarMar::Market do
 
+  let (:market1){
+    FarMar::Market.new({id: 1, name_of_market: "Eugene Farmers Market", address: "1300 Oak Street", city: "Eugene", county: "Lane", state: "Oregon", zip: "97405"})
+  }
+  let (:market2){
+    FarMar::Market.new({id: 502, name_of_market: "Corvallis Farmers Market", address: "1300 Oak Street", city: "Corvallis", county: "Lane", state: "Oregon", zip: "97405"})
+  }
+  let (:market_all?) {FarMar::Market.all?}
+
 #1
   it "Makes sure you can create an instance of market with various attributes" do
-    market1 = FarMar::Market.new({id: 501, name_of_market: "Eugene Farmers Market", address: "1300 Oak Street", city: "Eugene", county: "Lane", state: "Oregon", zip: "97405"})
-    expect(market1.id).must_equal(501)
+    expect(market1.id).must_equal(1)
     expect(market1.name_of_market).must_equal("Eugene Farmers Market")
     expect(market1.address).must_equal("1300 Oak Street")
     expect(market1.state).must_equal("Oregon")
@@ -16,30 +23,20 @@ describe FarMar::Market do
 
 #2
   it "Makes sure you can return appropriate market when given id" do
-    FarMar::Market.all?
     random_number = rand(1..500)
     FarMar::Market.find(random_number)
     expect(random_number).must_equal(@id =  random_number)
   end
 
 # 3
-  it "Makes sure that multiple instances of market are an array" do
-    market1 = FarMar::Market.new({id: 501, name_of_market: "Eugene Farmers Market", address: "1300 Oak Street", city: "Eugene", county: "Lane", state: "Oregon", zip: "97405"})
-    market2 = FarMar::Market.new({id: 502, name_of_market: "Corvallis Farmers Market", address: "1300 Oak Street", city: "Corvallis", county: "Lane", state: "Oregon", zip: "97405"})
-    markets_array = [market1, market2]
-    expect(market1.class).must_equal(FarMar::Market)
+  it "Makes sure that file you read in is appropriate length, and that it is an array" do
+    markets_array = market_all?
     expect(markets_array.class).must_equal(Array)
+    expect(markets_array.length).must_equal(500)
   end
 
-# 4
-  it "Makes sure that file you read in is appropriate length" do
-    array = FarMar::Market.all?
-    expect(array.length).must_equal(500)
-  end
-
-#5
+#4
   it "Vendor method returns a list of vendors with market ID" do
-    market1 = FarMar::Market.new({id: 1, name_of_market: "Eugene Farmers Market", address: "1300 Oak Street", city: "Eugene", county: "Lane", state: "Oregon", zip: "97405"})
     vendors_array = []
     vendors_array = market1.vendors
     expect(vendors_array.length).must_equal(6)
